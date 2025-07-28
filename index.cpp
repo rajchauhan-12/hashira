65344f47fab665268e024c06db3683631698f00d
rajchuahn-12 <rajchuahan25112004@gmail.com>
2025-07-28 12:23:32 +0530

    updated

--- index.cpp
+++ index.cpp
@@ -1,63 +1,90 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <map>
 #include <string>
+#include <cmath>
+#include <map>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
+
+long long convertToDecimal(const string& value, int base) {
+    long long result = 0;
+    for (char ch : value) {
+        int digit;
+        if (isdigit(ch)) digit = ch - '0';
+        else digit = tolower(ch) - 'a' + 10;
+        result = result * base + digit;
+    }
+    return result;
+}
 
 struct Share {
     int x;
-    string word;
-    int y;
+    long long y;
 };
 
-void trimLine(string& line) {
-    line.erase(remove(line.begin(), line.end(), '"'), line.end());
-    line.erase(remove(line.begin(), line.end(), ','), line.end());
-    line.erase(remove(line.begin(), line.end(), '{'), line.end());
-    line.erase(remove(line.begin(), line.end(), '}'), line.end());
-    line.erase(remove(line.begin(), line.end(), '['), line.end());
-    line.erase(remove(line.begin(), line.end(), ']'), line.end());
+long long lagrangeInterpolation(const vector<Share>& shares, int k) {
+    long long secret = 0;
+    for (int i = 0; i < k; ++i) {
+        long double term = shares[i].y;
+        for (int j = 0; j < k; ++j) {
+            if (i != j) {
+                term *= (0.0 - shares[j].x) / (shares[i].x - shares[j].x);
+            }
+        }
+        secret += term;
+    }
+    return round(secret);
 }
 
-vector<Share> parseJSON(const string& filename) {
-    ifstream file(filename);
-    vector<Share> shares;
+int main() {
+    ifstream file("data.json");
     string line;
-    Share current;
+    map<int, Share> shareMap;
+    int n = 0, k = 0;
 
     while (getline(file, line)) {
-        trimLine(line);
-        stringstream ss(line);
-        string key, value;
+        line.erase(remove_if(line.begin(), line.end(), ::isspace), line.end());
 
-        while (getline(ss, key, ':') && getline(ss, value)) {
-            if (key.find("x") != string::npos) {
-                current.x = stoi(value);
-            } else if (key.find("word") != string::npos) {
-                current.word = value;
-            } else if (key.find("y") != string::npos) {
-                current.y = stoi(value);
-                shares.push_back(current); 
+        if (line.find("\"n\":") != string::npos) {
+            n = stoi(line.substr(line.find(":") + 1));
+        } else if (line.find("\"k\":") != string::npos) {
+            k = stoi(line.substr(line.find(":") + 1));
+        } else {
+            for (int i = 1; i <= 10; ++i) {
+                string id = to_string(i);
+                if (line.find("\"" + id + "\":") != string::npos) {
+                    Share s;
+                    s.x = i;
+                    string baseStr, valueStr;
+                    getline(file, line);
+                    baseStr = line.substr(line.find(":\"") + 2);
+                    baseStr = baseStr.substr(0, baseStr.find("\""));
+                    getline(file, line);
+                    valueStr = line.substr(line.find(":\"") + 2);
+                    valueStr = valueStr.substr(0, valueStr.find("\""));
+
+                    int base = stoi(baseStr);
+                    s.y = convertToDecimal(valueStr, base);
+                    shareMap[i] = s;
+                }
             }
         }
     }
 
-    return shares;
-}
-
-void printShares(const vector<Share>& shares) {
-    for (const auto& s : shares) {
-        cout << "x: " << s.x << ", word: " << s.word << ", y: " << s.y << endl;
+    vector<Share> shares;
+    for (int i = 1; i <= k; ++i) {
+        shares.push_back(shareMap[i]);
     }
-}
 
-int main() {
-    string filename = "data.json";
-    vector<Share> shares = parseJSON(filename);
-    printShares(shares);
+    cout << "Parsed " << k << " shares:\n";
+    for (const auto& s : shares) {
+        cout << "x: " << s.x << ", y: " << s.y << endl;
+    }
+
+    long long secret = lagrangeInterpolation(shares, k);
+    cout << "\nRecovered Secret: " << secret << endl;
+
     return 0;
 }
